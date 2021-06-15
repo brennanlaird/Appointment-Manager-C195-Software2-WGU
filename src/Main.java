a400968290c8c6c64e91d1680eae2ca8abf9e59e
brennanlaird <brennan.laird@gmail.com>
2021-06-15 00:35:18 -0600

    Initial Commit.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-/*
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -18,11 +17,10 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-
+        DBConnect.startConnection();
 
         launch(args);
     }
 }
-*/
